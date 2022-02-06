8dc1a7c0c94c282bc32eb500994af3b55ffa92ed
Matt Descoteaux <79809704+Discotech17@users.noreply.github.com>
2022-02-06 14:13:28 -0500

    Create Find_Record_Pass_Fields_Back.js

---
+++ Find_Record_Pass_Fields_Back.js
@@ -0,0 +1,23 @@
+var FieldReturnInclude = Class.create();
+FieldReturnInclude.prototype = Object.extendsObject(global.AbstractAjaxProcessor, {
+  
+  getRecordFunction: function() {
+    var record = this.getParamter('sysparm_rec'); // receive record param
+    var table = this.getParamater('sysparm_table'); // receive table param
+    
+    var company = new GlideRecord(table); // query on table param
+    company.addQuery('sys_id', record); // check record and sys_id
+    company.query();
+    
+    if (company.next()) {
+      return JSON.stringify({ // put location and phone number fields into a JSON to pass back to client
+        'address': company.getDisplayValue('location'),
+        'phone': company.getDisplayValue('phone')
+      });
+    }
+  },
+  type: 'FieldReturnInclude'
+});
+
+//Script Include
+// View Find_Record_Pass_Client for Client Script
